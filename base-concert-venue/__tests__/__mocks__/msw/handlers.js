6ab14cbd74ff4a8c2fdfb13b9d4dd88508521d85
Moonyoung Chu <cnansdud11@hanmail.net>
2023-09-19 23:22:16 +0900

    fea: 35. Different MSW test 연습

--- base-concert-venue/__tests__/__mocks__/msw/handlers.js
+++ base-concert-venue/__tests__/__mocks__/msw/handlers.js
@@ -5,7 +5,9 @@ import { readFakeData } from "@/__tests__/__mocks__/fakeData";
 export const handlers = [
   rest.get("http://localhost:3000/api/shows/:showId", async (req, res, ctx) => {
     const { fakeShows } = await readFakeData();
-    return res(ctx.json({ show: fakeShows[0] }));
+    const {showId} = req.params;
+
+    return res(ctx.json({ show: fakeShows[showId] }));
   }),
   rest.get("http://localhost:3000/api/users/:userId/reservations", async (req, res, ctx) => {
     const { fakeReservations } = await readFakeData();
--- base-concert-venue/__tests__/ui/reservation.test.tsx
+++ base-concert-venue/__tests__/ui/reservation.test.tsx
@@ -8,3 +8,17 @@ test("예약 페이지 - 가능한 좌석 수가 정확한지 테스트", async(
     const seatCountText = await screen.findByText(/10 seats left/i);
     expect(seatCountText).toBeInTheDocument();
 })
+
+test("예약페이지는 솔드아웃 메시지를 보여준다. 자리가 없을 때 ", async()=>{
+    render(<Reservation showId={1} submitPurchase={jest.fn()} />);
+
+    const soldOutMessage = await screen.findByRole('heading',{
+        name: /sold out/i,
+    });
+    expect(soldOutMessage).toBeInTheDocument();
+
+    const purchaseButton = screen.queryByRole('button',{name: /purchase/i });
+    expect(purchaseButton).not.toBeInTheDocument();
+
+
+})
